2a74655f19fd0533d91d2b9742dfcba1587d9281
PH7NTOM <83135349+PH7NTOMXD@users.noreply.github.com>
2023-12-06 16:18:36 +0500

    Delete pha7data directory

--- pha7data/script.js
+++
@@ -1,1 +0,0 @@
-const readButton=document.querySelector('.read-button');const popup=document.getElementById('popup');const closeButton=document.getElementById('close-button');readButton.addEventListener('click',()=>{popup.style.display='block';});closeButton.addEventListener('click',()=>{popup.style.display='none';});
